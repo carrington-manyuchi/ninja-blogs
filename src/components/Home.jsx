83649446ae332073e3ef61c5a36e3703ab855de4
carrington_johnson <carringtonmanyuchi263@gmail.com>
2022-07-24 20:27:54 +0200

    Added blog delete functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,16 +13,23 @@ const Home = () => {
       title: "Welcome Party",
       body: "Lorem ipsum",
       author: "Carrington",
-      id: 1,
+      id: 2,
     },
     {
       title: "Web dev top tips",
       body: "Lorem ipsum",
       author: "Josephine",
-      id: 1,
+      id: 3,
     },
   ]);
-  const handleDelete = (id) => {};
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    console.log("use Effect ran");
+  });
 
   return (
     <div className="home">
